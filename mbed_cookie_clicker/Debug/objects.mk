8c3bd2479e1dc4288017129e0a4cfc2890cd1c4f
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-02-18 22:57:15 +0900

    Gitignored /Debug/

--- mbed_cookie_clicker/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /Users/akihiro/LPCXpresso/workspace/mbed_cookie_clicker/mbed/TARGET_LPC1768/TOOLCHAIN_GCC_CR/cmsis_nvic.o /Users/akihiro/LPCXpresso/workspace/mbed_cookie_clicker/mbed/TARGET_LPC1768/TOOLCHAIN_GCC_CR/system_LPC17xx.o /Users/akihiro/LPCXpresso/workspace/mbed_cookie_clicker/mbed/TARGET_LPC1768/TOOLCHAIN_GCC_CR/board.o /Users/akihiro/LPCXpresso/workspace/mbed_cookie_clicker/mbed/TARGET_LPC1768/TOOLCHAIN_GCC_CR/retarget.o /Users/akihiro/LPCXpresso/workspace/mbed_cookie_clicker/mbed/TARGET_LPC1768/TOOLCHAIN_GCC_CR/startup_LPC17xx.o
-
-LIBS := -lmbed
-

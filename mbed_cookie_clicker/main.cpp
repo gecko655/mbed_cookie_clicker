fcb4cddf3ea9ea73224e72618976b015935ea8ab
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-02-19 00:09:12 +0900

    Implemented jerk detection.

--- mbed_cookie_clicker/main.cpp
+++ mbed_cookie_clicker/main.cpp
@@ -1,4 +1,5 @@
 #include "stdlib.h"
+#include "math.h"
 #include "mbed.h"
 #include "rtos.h"
 #include "MMA7660.h"
@@ -12,26 +13,34 @@ BusOut colorled(p23,p24,p25);
 Mutex cookie_mutex;
 long cookie=0;
 
-int lastX=0;
-int lastY=0;
-int lastZ=0;
-
-bool isShaked(int x, int y, int z){
-	if(x==lastX
-			&&y==lastY
-			&&z==lastZ){
-		return false;
+double lastX=0;
+double lastY=0;
+double lastZ=0;
+bool init=false;
+double distance(double x, double y, double z){
+	return sqrt(pow(lastX-x,2)+pow(lastY-y,2)+pow(lastZ-z,2));
+}
+bool thinOut=false;
+bool isShaked(double x, double y, double z){
+    bool ret=false;
+	if(!init){
+		init = true;
+	}else{
+        if(distance(x,y,z)>2.0){
+        	thinOut=!thinOut;
+            ret = thinOut;
+        }
 	}
 	lastX = x;
 	lastY = y;
 	lastZ = z;
-	return true;
+	return ret;
 }
-int sgn(double arg){
-	return
-			arg==0? 0 :
-            arg> 0? 1 :
-			-1;
+void lightChange(){
+    colorled=colorled+1;
+    if(colorled==8){
+        colorled=1;
+    }
 }
 
 void bakeCookie(){
@@ -39,6 +48,7 @@ void bakeCookie(){
     cookie++;
     lcd.locate(0,0);
     lcd.printf("%d",cookie);
+    lightChange();
     cookie_mutex.unlock();
 }
 
@@ -52,34 +62,16 @@ void mma_read(void const* args){
     	double x = MMA.x();
     	double y = MMA.y();
     	double z = MMA.z();
-    	double ax = fabs(x);
-    	double ay = fabs(y);
-    	double az = fabs(z);
-    	if(ax>1.3&&ay>1.3&&az<=0.5){
-    		if(isShaked(sgn(x),sgn(y),0)){
-    			bakeCookie();
-    		}
-    	}else if(ay>1.3&&az>1.3&&ax<=0.5){
-    		if(isShaked(0,sgn(y),sgn(z))){
-    			bakeCookie();
-    		}
-    	}else if(az>1.3&&ax>1.3&&ay<=0.5){
-    		if(isShaked(sgn(x),0,sgn(z))){
-    			bakeCookie();
-    		}
+        if(isShaked(x,y,z)){
+            bakeCookie();
     	}
-    	Thread::wait(10);
+    	Thread::wait(100);
     }
 }
-
 int main() {
 	Thread mma_thread(mma_read);
     colorled=0;
     while (true) {
-        colorled=colorled+1;
-        if(colorled==8){
-            colorled=0;
-        }
-        Thread::wait(500);
+    	Thread::wait(1000);
     }
 }

302982e94db7f68fd5a33650b23f3e2bb8573160
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-02-19 20:04:26 +0900

    Added tippy.h

--- mbed_cookie_clicker/main.cpp
+++ mbed_cookie_clicker/main.cpp
@@ -5,58 +5,92 @@
 #include "MMA7660.h"
 #include "C12832.h"
 
+#include "tippy.h"
+
 DigitalOut myled(LED1);
+InterruptIn button(p14);
 MMA7660 MMA(p28, p27);
 C12832 lcd(p5, p7, p6, p8, p11);
 BusOut colorled(p23, p24, p25);
 
 Mutex cookie_mutex;
+Mutex print_mutex;
 long cookie = 0;
+int tippy;
+int cost;
 
-double lastX = 0;
-double lastY = 0;
-double lastZ = 0;
-bool init = false;
-double distance(double x, double y, double z) {
-    return sqrt(pow(lastX - x, 2) + pow(lastY - y, 2) + pow(lastZ - z, 2));
+Timer t;
+double lastAX = 0;
+double lastAY = 0;
+double lastAZ = 0;
+int lastTime = t.read_ms();
+
+inline double square(double x) {
+    return x*x;
 }
-bool thinOut = false;
-bool isShaked(double x, double y, double z) {
+
+inline double getJerk(double x, double y, double z, int time) {
+    return sqrt(square(lastAX - x) + square(lastAY - y) + square(lastAZ - z))/(time-lastTime)*1000;
+}
+bool schmittTriger = false;
+double thresholdUpper = 100;//[m/s/s/s]
+double thresholdLower = 0.01;//[m/s/s/s]
+bool isShaked(double ax, double ay, double az) {
     bool ret = false;
-    if (!init) {
-        init = true;
-    } else {
-        if (distance(x, y, z) > 2.0) {
-            thinOut = !thinOut;
-            ret = thinOut;
-        }
+    int time = t.read_ms();
+    if (getJerk(ax, ay, az, time) > thresholdUpper && !schmittTriger) {
+        schmittTriger=true;
+        ret = true;
+    } else if (getJerk(ax, ay, az, time) < thresholdLower && schmittTriger) {
+        schmittTriger=false;
+        ret = false;
     }
-    lastX = x;
-    lastY = y;
-    lastZ = z;
+    lastAX = ax;
+    lastAY = ay;
+    lastAZ = az;
+    lastTime = time;
     return ret;
 }
 void lightChange() {
+    printf("%d\n",colorled.read());
+    colorled = colorled + 1;
     if (colorled.read() == 7) {
         colorled = 0;
     }
-    colorled = colorled + 1;
+}
+void print(){
+    print_mutex.lock();
+    lcd.cls();
+    lcd.locate(0, 0);
+    lcd.printf("Cookie: %d\n", cookie);
+    lcd.locate(64, 0);
+    lcd.printf("Tippy cost:%d",cost);
+    lcd.locate(64, 0);
+    lcd.printf("Tippy cost:%d",cost);
+    lcd.print_bm(tippyBmp,1+(cookie-1)*tippyBmp.xSize,16);
+    lcd.copy_to_lcd();
+    print_mutex.unlock();
 }
 
 void bakeCookie() {
     cookie_mutex.lock();
     cookie++;
-    lcd.locate(0, 0);
-    lcd.printf("%d", cookie);
+    print();
     lightChange();
     cookie_mutex.unlock();
 }
 
+void tippy_buy() {
+    cookie_mutex.lock();
+    if(cookie>=cost){
+        cookie-=cost;
+        tippy++;
+        cost+=tippy*10;
+        print();
+    }
+    cookie_mutex.unlock();
+}
 void mma_read(void const* args) {
-    if (MMA.testConnection())
-        printf("Successfully connected MMA");
-    else
-        printf("Failed to connect MMA");
     while (1) {
         double x = MMA.x();
         double y = MMA.y();
@@ -64,12 +98,27 @@ void mma_read(void const* args) {
         if (isShaked(x, y, z)) {
             bakeCookie();
         }
-        Thread::wait(100);
+        Thread::wait(10);
     }
+}
+void init(){
+    t.start();
+    if (MMA.testConnection())
+        printf("Successfully connected MMA");
+    else
+        printf("Failed to connect MMA");
+    lastAX = MMA.x();
+    lastAY = MMA.y();
+    lastAZ = MMA.z();
+    lastTime = t.read_ms();
+    colorled = 7;
+    tippy = 0;
+    cost=10;
 }
 int main() {
     Thread mma_thread(mma_read);
-    colorled = 0;
+    button.rise(tippy_buy);
+    init();
     while (true) {
         Thread::wait(1000);
     }
